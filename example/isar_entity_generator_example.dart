edf5a6ee0346b3293e7d173034557881f28ed3a2
Vicente Russo <vicente.russo@gmail.com>
2023-05-11 16:32:23 -0300

    removed test temporarily

--- example/isar_entity_generator_example.dart
+++ example/isar_entity_generator_example.dart
@@ -1,6 +1,6 @@
 import 'package:isar_entity_generator/isar_entity_generator.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  // var awesome = Awesome();
+  // print('awesome: ${awesome.isAwesome}');
 }
